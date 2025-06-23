6b9ba05f549169bb60bbc6943d5850bcd4c21ca6
Tran Phuc Tien <phuctienrt@gmail.com>
2025-06-23 17:12:12 +0700

    [fix] Update CRUD Product #2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from './contexts/AuthContext';
