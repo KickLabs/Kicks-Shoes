e8c42faefa6dd92206a24fd3313779c82a17ca6f
Nguyen Hue Suong <suongnhde180706@fpt.edu.vn>
2025-06-30 05:53:21 +0700

    [KICKS-26][BE][fix] Fix api url, add to cart

--- frontend/src/components/pages/cart/cartService.js
+++ frontend/src/components/pages/cart/cartService.js
@@ -1,11 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axiosInstance from '../../../services/axiosInstance';
 
-const API_URL = 'http://localhost:3000/api/cart';
-
 export const getCart = createAsyncThunk('cart/get', async (_, thunkAPI) => {
   try {
-    const res = await axiosInstance.get('/');
+    const res = await axiosInstance.get('/cart/');
     return res.data;
   } catch (error) {
     console.log('looi', error.response?.data?.message || error.message);
@@ -15,7 +13,7 @@ export const getCart = createAsyncThunk('cart/get', async (_, thunkAPI) => {
 
 export const addOrUpdateCartItem = createAsyncThunk('cart/addOrUpdate', async (item, thunkAPI) => {
   try {
-    const res = await axiosInstance.post('/items', item);
+    const res = await axiosInstance.post('/cart/items', item);
     return res.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.response?.data?.message || error.message);
@@ -26,7 +24,7 @@ export const updateCartItem = createAsyncThunk(
   'cart/updateItem',
   async ({ itemId, updateData }, thunkAPI) => {
     try {
-      const res = await axiosInstance.put(`/items/${itemId}`, updateData);
+      const res = await axiosInstance.put(`/cart/items/${itemId}`, updateData);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response?.data?.message || error.message);
@@ -36,7 +34,7 @@ export const updateCartItem = createAsyncThunk(
 
 export const removeCartItem = createAsyncThunk('cart/removeItem', async (itemId, thunkAPI) => {
   try {
-    const res = await axiosInstance.delete(`/items/${itemId}`);
+    const res = await axiosInstance.delete(`/cart/items/${itemId}`);
     return res.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.response?.data?.message || error.message);
