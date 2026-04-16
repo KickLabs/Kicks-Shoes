68ca575441ff529c816a6fe411b7cdcc2f32f144
Face Attendance System <attendance@system.local>
2026-04-16 16:19:18 +0700

    chore(terraform): remove all tracked tf files

--- infra/terraform/modules/autoscaling/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "autoscaling_target_resource_id" {
-  description = "Autoscaling target resource ID"
-  value       = aws_appautoscaling_target.ecs.resource_id
-}
-
-output "autoscaling_policy_arn" {
-  description = "Autoscaling policy ARN"
-  value       = aws_appautoscaling_policy.cpu_target_tracking.arn
-}
